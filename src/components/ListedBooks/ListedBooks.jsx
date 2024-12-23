229f7328b08217b21683ecb96a051ed3500911a0
Amit biswas <amitbiswas9282@gmail.com>
2024-12-23 12:56:47 +0600

    listed added

--- src/components/ListedBooks/ListedBooks.jsx
+++ src/components/ListedBooks/ListedBooks.jsx
@@ -1,4 +1,6 @@
 import { useLoaderData, useParams } from "react-router-dom";
+import { addToStoredReadList } from "../../utiliti/addToReadList";
+import { addToStoredWishList } from "../../utiliti/addToWishList";
 
 const ListedBooks = () => {
   const { bookId } = useParams();
@@ -7,6 +9,7 @@ const ListedBooks = () => {
 
   const book = data.find((book) => book.bookId === id);
   const {
+    bookId: currentBookId,
     bookName,
     author,
     image,
@@ -19,51 +22,55 @@ const ListedBooks = () => {
     yearOfPublishing,
   } = book;
 
+  const handleAsRead = (id) => {
+    addToStoredReadList(id)
+  }
+  const handleAsWishList = (id) => {
+    addToStoredWishList(id)
+  }
+
   return (
-    <div className="container mx-auto mb-6">
-      <h2 className="text-3xl">ListedBooks: {bookId}</h2>
-      <div className="grid grid-cols-2 gap-6">
-        <div className="w-full">
-          <img className="w-full h-[564px] bg-cover" src={image} alt="" />
+    <div className="container mx-auto mb-6 grid grid-cols-2 gap-6">
+      <div className="w-full">
+        <img className="w-full h-[564px] bg-cover" src={image} alt="" />
+      </div>
+      <div>
+        <h2 className="text-4xl font-bold">{bookName}</h2>
+        <p className="text-base my-2">By : {author}</p>
+        <hr className="black" />
+        <p className="my-2">Fiction</p>
+        <hr className="black" />
+        <h2 className="my-2 font-semibold">
+          Review: <span className="text-base font-light">{review}</span>
+        </h2>
+        <div className="flex gap-4">
+          <h2 className="font-semibold">Tag : </h2>
+          {tags.map((tag, index) => (
+            <p key={index} className="text-green-500 bg-green-100 p-1 rounded-xl">{tag}</p>
+          ))}
         </div>
-        <div>
-          <h2 className="text-4xl font-bold">{bookName}</h2>
-          <p className="text-base my-2">By : {author}</p>
-          <hr className="black" />
-          <p className="my-2">Fiction</p>
-          <hr className="black" />
-          <h2 className="my-2 font-semibold">
-            Review: <span className="text-base font-light">{review}</span>
-          </h2>
-          <div className="flex gap-4">
-            <h2 className="font-semibold">Tag : </h2>
-            {tags.map((tag) => (
-              <p className="text-green-500 bg-green-100 p-1 rounded-xl">{tag}</p>
-            ))}
+        <hr className="black my-2" />
+        <div className="w-64">
+          <div className="flex justify-between">
+            <p>Number of Pages:</p>
+            <p className="font-semibold">{totalPages}</p>
           </div>
-          <hr className="black my-2" />
-          <div className="w-64">
-            <div className="flex justify-between">
-              <p>Number of Pages:</p>
-              <p className="font-semibold">{totalPages}</p>
-            </div>
-            <div className="flex justify-between my-2">
-              <p>Publisher:</p>
-              <p className="font-semibold">{publisher}</p>
-            </div>
-            <div className="flex justify-between">
-              <p>Year of Publishing:</p>
-              <p className="font-semibold">{yearOfPublishing}</p>
-            </div>
-            <div className="flex justify-between my-2">
-              <p>Rating:</p>
-              <p className="font-semibold">{rating}</p>
-            </div>
+          <div className="flex justify-between my-2">
+            <p>Publisher:</p>
+            <p className="font-semibold">{publisher}</p>
           </div>
-          <div className="flex gap-6">
-            <button className="btn btn-outline btn-accent">Accent</button>
-            <button className="btn btn-success">Success</button>
+          <div className="flex justify-between">
+            <p>Year of Publishing:</p>
+            <p className="font-semibold">{yearOfPublishing}</p>
           </div>
+          <div className="flex justify-between my-2">
+            <p>Rating:</p>
+            <p className="font-semibold">{rating}</p>
+          </div>
+        </div>
+        <div className="flex gap-6">
+          <button onClick={()=> handleAsRead(currentBookId)} className="btn btn-outline btn-accent">Read</button>
+          <button onClick={()=> handleAsWishList(currentBookId)} className="btn btn-success">Wishlist</button>
         </div>
       </div>
     </div>
---
+++ src/utiliti/addToWishList.js
@@ -0,0 +1,24 @@
+const getStoredWishList = () => {
+    const storedListStr = localStorage.getItem("wish-list");
+    if (storedListStr) {
+      const storedList = JSON.parse(storedListStr);
+      return storedList;
+    } else {
+      return [];
+    }
+  };
+  
+  const addToStoredWishList = (id) => {
+    const storedList = getStoredWishList();
+    if (storedList.includes(id)) {
+      // already exist. do not add it
+      console.log(id, "already exists, don't add it to the read list.");
+    } else {
+      storedList.push(id);
+      const storedListStr = JSON.stringify(storedList);
+      localStorage.setItem("wish-list", storedListStr); 
+    }
+  };
+  
+  export { addToStoredWishList };
+  
