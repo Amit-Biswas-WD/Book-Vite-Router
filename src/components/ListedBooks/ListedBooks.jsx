59f6bd937b6334baebb03ec75af24a35a29338e8
Amit biswas <amitbiswas9282@gmail.com>
2024-12-24 23:39:22 +0600

    sort complete

--- src/components/ListedBooks/ListedBooks.jsx
+++ src/components/ListedBooks/ListedBooks.jsx
@@ -4,6 +4,7 @@ import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
 import "react-tabs/style/react-tabs.css";
 import { getStoredReadList } from "../../utiliti/addToReadList";
 import { getStoredWishList } from "../../utiliti/addToWishList";
+import Book from "../Book/Book";
 
 const ListedBooks = () => {
   const [readList, setReadList] = useState([]);
@@ -34,18 +35,32 @@ const ListedBooks = () => {
   }, []);
 
   const handleSort = (sortType) => {
-    setSort(sortType)
+    setSort(sortType);
+
+    if (sortType === "No of Page.") {
+      const sortedReadList = [...readList].sort(
+        (a, b) => b.totalPages - a.totalPages
+      );
+      setReadList(sortedReadList);
+    }
+
+    if (sortType === "Rating") {
+      const sortedReadList = [...readList].sort((a, b) => a.rating - b.rating);
+      setReadList(sortedReadList);
+    }
   };
 
   return (
     <div className="container mx-auto">
       <details className="dropdown">
-        <summary className="btn m-1">{sort ? `Sort By ${sort}` : "Sort By"}</summary>
+        <summary className="btn m-1">
+          {sort ? `Sort By ${sort}` : "Sort By"}
+        </summary>
         <ul className="menu dropdown-content bg-base-100 rounded-box z-[1] w-52 p-2 shadow">
-          <li onClick={()=> handleSort("Rating")}>
-          <a> Rating</a>
+          <li onClick={() => handleSort("Rating")}>
+            <a> Rating</a>
           </li>
-          <li  onClick={()=> handleSort("No of Page.")}>
+          <li onClick={() => handleSort("No of Page.")}>
             <a>No of Page.</a>
           </li>
         </ul>
@@ -58,9 +73,15 @@ const ListedBooks = () => {
           </TabList>
           <TabPanel>
             <h2>Read List: {readList.length}</h2>
+            {readList.map((book) => (
+              <Book className="my-4" key={book.bookId} book={book} />
+            ))}
           </TabPanel>
           <TabPanel>
             <h2>Wish List: {wishList.length}</h2>
+              {wishList.map((book) => (
+                <Book key={book.bookId} book={book} />
+              ))}
           </TabPanel>
         </Tabs>
       </h2>
